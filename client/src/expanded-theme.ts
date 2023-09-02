355de005477a25d13ffb2cd74c00fc2366739fd6
adenugbamichael <adenugbamicke@gmail.com>
2023-09-02 20:12:56 +0100

    fix prod

--- client/src/expanded-theme.ts
+++ client/src/expanded-theme.ts
@@ -2,11 +2,19 @@
 import { Palette, PaletteColor } from "@mui/material/styles/createPalette"
 
 declare module "@mui/material/styles/createPalette" {
-  export interface PaletteColor {
+  interface PaletteColor {
     [key: number]: string
   }
 
-  export interface Palette {
+  interface Palette {
     tertiary: PaletteColor
+  }
+}
+
+// Extend color prop on components
+declare module "@mui/material/Chip" {
+  export interface ChipPropsColorOverrides {
+    tertiary: PaletteColor
+    [key: number]: string
   }
 }
